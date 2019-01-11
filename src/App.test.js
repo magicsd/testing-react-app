4165c3052c0b6efffdcfe6b46291a1d826d2e965
Alex <sd@alexdus.com>
2019-01-12 00:00:09 +0300

    Add Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
 
 test('Renders without error', () => {
 
