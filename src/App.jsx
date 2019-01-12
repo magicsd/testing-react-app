218a1b8b109bf5681237dbef58471014845883b5
Alex <sd@alexdus.com>
2019-01-12 19:35:01 +0300

    Implement simple counter app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      counterValue: 0,
+    };
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({
+      counterValue: this.state.counterValue + 1,
+    });
+  }
+
   render() {
+    const { counterValue } = this.state;
     return (
-      <div>
-        React App
+      <div className="app">
+        <h1>
+          The count is { counterValue }
+        </h1>
+        <button onClick={this.handleIncrement}>
+          Increment counter
+        </button>
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
@@ -8,5 +8,5 @@ Enzyme.configure({
 });
 
 test('Renders without error', () => {
-
+  const wrapper = shallow(<App />);
 });
