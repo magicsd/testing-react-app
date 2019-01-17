f3c0fefa38a1226c307c1839b7121d6d13eb5d0c
Alex <sd@alexdus.com>
2019-01-17 21:45:04 +0300

    Make congrats test green

--- src/components/Congrats.jsx
+++ src/components/Congrats.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Congrats = () => (
-  <div>
-    Congrats
-  </div>
-);
+const Congrats = ({ success }) => {
+  if (!success) {
+    return <div data-test="component-congrats" />
+  }
+
+  return (
+    <div data-test="component-congrats">
+      <span data-test="congrats-message">
+        Congratulations! You guessed the word!
+      </span>
+    </div>
+  );
+};
 
 export default Congrats;
