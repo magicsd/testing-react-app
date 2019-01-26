26fbd3fb732a66b830783cbfdcbb863c0bc44750
Alex <sd@alexdus.com>
2019-01-26 19:10:47 +0300

    Add tests and implement code for GuessedWords

---
+++ src/components/GuessedWords.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = ({ guessedWords }) => {
+  let contents;
+
+  if (!guessedWords.length) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secret word!
+      </span>
+    );
+  } else {
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+          <tr>
+            <th>Guess</th>
+            <th>Matching Letters</th>
+          </tr>
+          </thead>
+          <tbody>
+          { guessedWords.map(word => (
+            <tr key={word.guessedWord} data-test="guessed-word">
+              <td>
+                { word.guessedWord }
+              </td>
+              <td>
+                { word.letterMatchCount }
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return (
+    <div data-test="component-guessed-words">
+      { contents }
+    </div>
+  );
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default GuessedWords;
---
+++ src/tests/GuestWords.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../../test/testUtils';
+import GuessedWords from '../components/GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+describe('GuessedWords test', () => {
+  test('Does not throw a warning with expected props', () => {
+    checkProps(GuessedWords, defaultProps);
+  });
+
+  describe('If there are no words guessed', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = setup({ guessedWords: [] });
+    });
+
+    test('Renders without errors', () => {
+      const component = findByTestAttr(wrapper, 'component-guessed-words');
+      expect(component.length).toBe(1);
+    });
+
+    test('Renders instructions to guess a word', () => {
+      const guessInstructions = findByTestAttr(wrapper, 'guess-instructions');
+      expect(guessInstructions.text()).toBeTruthy();
+    });
+  });
+
+  describe('If there are words guessed', () => {
+    const guessedWords = [
+      { guessedWord: 'train', letterMatchCount: 3 },
+      { guessedWord: 'agile', letterMatchCount: 1 },
+      { guessedWord: 'party', letterMatchCount: 5 },
+    ];
+
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = setup({ guessedWords })
+    });
+
+    test('Renders without errors', () => {
+      const component = findByTestAttr(wrapper, 'component-guessed-words');
+      expect(component.length).toBe(1);
+    });
+
+    test('Renders "guessed words" section', () => {
+      const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+      expect(guessedWordsNode.length).toBe(1);
+    });
+
+    test('Renders correct number of guessed words', () => {
+      const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+      expect(guessedWordsNodes.length).toBe(guessedWords.length);
+    });
+  });
+});
